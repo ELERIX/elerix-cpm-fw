ebe92cd11a139fd30589dde82f9b070ca6035ca3
sedlons <sedlons451@gmail.com>
2021-01-05 15:40:18 +0100

    Created MPLAB project

--- NewUnit_0.cp
+++
@@ -1,1 +0,0 @@
-#line 1 "C:/ubiprojects/GWL_CPM/version_fianlni_upravene108_062020/NewUnit_0.c"
---
+++ elerix-cpm-fw.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=elerix-cpm-fw.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/elerix-cpm-fw.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=elerix-cpm-fw.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/elerix-cpm-fw.x.tar
